d1c3cc82efe3e36589d7afd1dd7b59fa13105f92
Rob Nelson <nexisentertainment@gmail.com>
2025-06-29 01:05:39 -0700

    Code cleanup via Rider

--- See_who_is_talking.cs
+++ See_who_is_talking.cs
@@ -1,85 +1,61 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
 
-namespace See_who_is_talking
+namespace See_who_is_talking;
+
+// Token: 0x02000007 RID: 7
+[BepInPlugin("XiaoLei_See_who_is_talking", "See_who_is_talking", "1.0")]
+public class See_who_is_talking : BaseUnityPlugin
 {
-	// Token: 0x02000007 RID: 7
-	[BepInPlugin("XiaoLei_See_who_is_talking", "See_who_is_talking", "1.0")]
-	public class See_who_is_talking : BaseUnityPlugin
-	{
-		// Token: 0x17000001 RID: 1
-		// (get) Token: 0x06000008 RID: 8 RVA: 0x000020D4 File Offset: 0x000002D4
-		// (set) Token: 0x06000009 RID: 9 RVA: 0x000020DB File Offset: 0x000002DB
-		internal static See_who_is_talking Instance { get; private set; }
+    // Token: 0x17000001 RID: 1
+    // (get) Token: 0x06000008 RID: 8 RVA: 0x000020D4 File Offset: 0x000002D4
+    // (set) Token: 0x06000009 RID: 9 RVA: 0x000020DB File Offset: 0x000002DB
+    internal static See_who_is_talking Instance { get; private set; }
 
-		// Token: 0x17000002 RID: 2
-		// (get) Token: 0x0600000A RID: 10 RVA: 0x000020E3 File Offset: 0x000002E3
-		internal static ManualLogSource Logger
-		{
-			get
-			{
-				return See_who_is_talking.Instance._logger;
-			}
-		}
+    // Token: 0x17000002 RID: 2
+    // (get) Token: 0x0600000A RID: 10 RVA: 0x000020E3 File Offset: 0x000002E3
+    internal static ManualLogSource Logger => Instance._logger;
 
-		// Token: 0x17000003 RID: 3
-		// (get) Token: 0x0600000B RID: 11 RVA: 0x000020EF File Offset: 0x000002EF
-		private ManualLogSource _logger
-		{
-			get
-			{
-				return base.Logger;
-			}
-		}
+    // Token: 0x17000003 RID: 3
+    // (get) Token: 0x0600000B RID: 11 RVA: 0x000020EF File Offset: 0x000002EF
+    private ManualLogSource _logger => base.Logger;
 
-		// Token: 0x17000004 RID: 4
-		// (get) Token: 0x0600000C RID: 12 RVA: 0x000020F7 File Offset: 0x000002F7
-		// (set) Token: 0x0600000D RID: 13 RVA: 0x000020FF File Offset: 0x000002FF
-		internal Harmony? Harmony
-		{
-			get;
-			set;
-		}
+    // Token: 0x17000004 RID: 4
+    // (get) Token: 0x0600000C RID: 12 RVA: 0x000020F7 File Offset: 0x000002F7
+    // (set) Token: 0x0600000D RID: 13 RVA: 0x000020FF File Offset: 0x000002FF
+    internal Harmony? Harmony { get; set; }
 
-		// Token: 0x0600000E RID: 14 RVA: 0x00002108 File Offset: 0x00000308
-		private void Awake()
-		{
-			See_who_is_talking.Instance = this;
-			base.gameObject.transform.parent = null;
-			base.gameObject.hideFlags = HideFlags.HideAndDontSave;
-			if (UnityEngine.Object.FindObjectOfType<VoiceDisplayManager>() == null)
-			{
-				GameObject go = new GameObject("VoiceDisplay");
-				UnityEngine.Object.DontDestroyOnLoad(go);
-				go.hideFlags = HideFlags.HideAndDontSave;
-				go.AddComponent<VoiceDisplayManager>();
-			}
-			this.Patch();
-			See_who_is_talking.Logger.LogInfo(string.Format("{0} v{1} has loaded!", base.Info.Metadata.GUID, base.Info.Metadata.Version));
-		}
+    // Token: 0x0600000E RID: 14 RVA: 0x00002108 File Offset: 0x00000308
+    private void Awake()
+    {
+        Instance = this;
+        gameObject.transform.parent = null;
+        gameObject.hideFlags = HideFlags.HideAndDontSave;
+        if (FindObjectOfType<VoiceDisplayManager>() == null)
+        {
+            var go = new GameObject("VoiceDisplay");
+            DontDestroyOnLoad(go);
+            go.hideFlags = HideFlags.HideAndDontSave;
+            go.AddComponent<VoiceDisplayManager>();
+        }
 
-		// Token: 0x0600000F RID: 15 RVA: 0x000021AC File Offset: 0x000003AC
-		internal void Patch()
-		{
-			if (this.Harmony == null)
-			{
-				this.Harmony = new Harmony(base.Info.Metadata.GUID);
-			}
-			this.Harmony.PatchAll();
-		}
+        Patch();
+        Logger.LogInfo(string.Format("{0} v{1} has loaded!", Info.Metadata.GUID, Info.Metadata.Version));
+    }
 
-		// Token: 0x06000010 RID: 16 RVA: 0x000021EC File Offset: 0x000003EC
-		internal void Unpatch()
-		{
-			Harmony harmony = this.Harmony;
-			if (harmony != null)
-			{
-				harmony.UnpatchSelf();
-			}
-		}
-	}
+    // Token: 0x0600000F RID: 15 RVA: 0x000021AC File Offset: 0x000003AC
+    internal void Patch()
+    {
+        if (Harmony == null) Harmony = new Harmony(Info.Metadata.GUID);
+        Harmony.PatchAll();
+    }
+
+    // Token: 0x06000010 RID: 16 RVA: 0x000021EC File Offset: 0x000003EC
+    internal void Unpatch()
+    {
+        var harmony = Harmony;
+        if (harmony != null) harmony.UnpatchSelf();
+    }
 }
--- VoiceDisplayManager.cs
+++ VoiceDisplayManager.cs
@@ -1,144 +1,131 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
-namespace See_who_is_talking
+namespace See_who_is_talking;
+
+// Token: 0x02000009 RID: 9
+public class VoiceDisplayManager : MonoBehaviour
 {
-	// Token: 0x02000009 RID: 9
-	public class VoiceDisplayManager : MonoBehaviour
-	{
-		// Token: 0x06000014 RID: 20 RVA: 0x000022BC File Offset: 0x000004BC
-		private void Awake()
-		{
-			if (VoiceDisplayManager.instance != null && VoiceDisplayManager.instance != this)
-			{
-				UnityEngine.Object.Destroy(base.gameObject);
-			}
-			else
-			{
-				VoiceDisplayManager.instance = this;
-				UnityEngine.Object.DontDestroyOnLoad(base.gameObject);
-			}
-		}
+    // Token: 0x04000009 RID: 9
+    private const float displayDuration = 2f;
 
-		// Token: 0x06000015 RID: 21 RVA: 0x0000230C File Offset: 0x0000050C
-		public static void SetSpeakingPlayer(string name)
-		{
-			if (VoiceDisplayManager.instance == null || string.IsNullOrWhiteSpace(name)) return;
-			name = name.Trim();
-			object obj = VoiceDisplayManager.instance.dictLock;
-			lock (obj)
-			{
-				bool flag3 = string.IsNullOrWhiteSpace(name);
-				if (flag3)
-				{
-					See_who_is_talking.Logger.LogWarning("SetSpeakingPlayer called with null or empty name.");
-				}
-				else
-				{
-					bool flag4 = VoiceDisplayManager.instance.speakingPlayers.ContainsKey(name);
-					if (flag4)
-					{
-						VoiceDisplayManager.instance.speakingPlayers[name] = 2f;
-					}
-					else
-					{
-						VoiceDisplayManager.instance.speakingPlayers.Add(name, 2f);
-					}
-				}
-			}
-		}
+    // Token: 0x04000006 RID: 6
+    private static VoiceDisplayManager? instance;
 
-		// Token: 0x06000016 RID: 22 RVA: 0x000023D8 File Offset: 0x000005D8
-		private void Update()
-		{
-			lock (dictLock)
-			{
-				List<string> keys = new List<string>(this.speakingPlayers.Keys);
-				foreach (string key in keys)
-				{
-					if (string.IsNullOrWhiteSpace(key))
-					{
-						this.speakingPlayers.Remove(key);
-					}
-					else
-					{
-						speakingPlayers[key] -= Time.deltaTime;
-						bool flag3 = this.speakingPlayers[key] <= 0f;
-						if (flag3)
-						{
-							this.speakingPlayers.Remove(key);
-						}
-					}
-				}
-			}
-		}
+    // Token: 0x04000007 RID: 7
+    private readonly object dictLock = new();
 
-		// Token: 0x06000017 RID: 23 RVA: 0x000024D4 File Offset: 0x000006D4
-		private void OnGUI()
-		{
-			try
-			{
-				List<KeyValuePair<string, float>> displayList;
-				lock (dictLock)
-				{
-					bool flag2 = this.speakingPlayers.Count == 0;
-					if (flag2)
-					{
-						return;
-					}
-					displayList = new List<KeyValuePair<string, float>>(this.speakingPlayers);
-				}
-				displayList.Reverse();
-				GUIStyle style = new GUIStyle(GUI.skin.label)
-				{
-					fontSize = 21,
-					alignment = (UnityEngine.TextAnchor)5,
-					wordWrap = false,
-					normal = 
-					{
-						textColor = Color.yellow
-					}
-				};
-				float padding = 2f;
-				float height = 40f;
-				float yStart = (float)Screen.height - height - 20f;
-				int index = 0;
-				foreach (KeyValuePair<string, float> kvp in displayList)
-				{
-					string player = kvp.Key;
-					float timeLeft = kvp.Value;
-					bool flag3 = string.IsNullOrWhiteSpace(player);
-					if (!flag3)
-					{
-						float alpha = Mathf.Clamp01(timeLeft / 2f);
-						style.normal.textColor = new Color(1f, 0.8f, 0f, alpha);
-						string text = player + " is speaking...";
-						Vector2 textSize = style.CalcSize(new GUIContent(text));
-						float x = (float)(Screen.width - 20) - textSize.x;
-						float y = yStart - (float)index * (height + padding);
-						GUI.Label(new Rect(x, y, textSize.x, height), text, style);
-						index++;
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				See_who_is_talking.Logger.LogError(string.Format("[VoiceDisplayManager.OnGUI] Exception: {0}", ex));
-			}
-		}
+    // Token: 0x04000008 RID: 8
+    private readonly Dictionary<string, float> speakingPlayers = new();
 
-		// Token: 0x04000006 RID: 6
-		private static VoiceDisplayManager? instance;
+    // Token: 0x06000014 RID: 20 RVA: 0x000022BC File Offset: 0x000004BC
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
-		// Token: 0x04000007 RID: 7
-		private readonly object dictLock = new object();
+    // Token: 0x06000016 RID: 22 RVA: 0x000023D8 File Offset: 0x000005D8
+    private void Update()
+    {
+        lock (dictLock)
+        {
+            List<string> keys = new(speakingPlayers.Keys);
+            foreach (var key in keys)
+                if (string.IsNullOrWhiteSpace(key))
+                {
+                    speakingPlayers.Remove(key);
+                }
+                else
+                {
+                    speakingPlayers[key] -= Time.deltaTime;
+                    var flag3 = speakingPlayers[key] <= 0f;
+                    if (flag3) speakingPlayers.Remove(key);
+                }
+        }
+    }
 
-		// Token: 0x04000008 RID: 8
-		private readonly Dictionary<string, float> speakingPlayers = new Dictionary<string, float>();
+    // Token: 0x06000017 RID: 23 RVA: 0x000024D4 File Offset: 0x000006D4
+    private void OnGUI()
+    {
+        try
+        {
+            List<KeyValuePair<string, float>> displayList;
+            lock (dictLock)
+            {
+                var flag2 = speakingPlayers.Count == 0;
+                if (flag2) return;
+                displayList = new List<KeyValuePair<string, float>>(speakingPlayers);
+            }
 
-		// Token: 0x04000009 RID: 9
-		private const float displayDuration = 2f;
-	}
+            displayList.Reverse();
+            var style = new GUIStyle(GUI.skin.label)
+            {
+                fontSize = 21,
+                alignment = (TextAnchor)5,
+                wordWrap = false,
+                normal =
+                {
+                    textColor = Color.yellow
+                }
+            };
+            var padding = 2f;
+            var height = 40f;
+            var yStart = Screen.height - height - 20f;
+            var index = 0;
+            foreach (KeyValuePair<string, float> kvp in displayList)
+            {
+                var player = kvp.Key;
+                var timeLeft = kvp.Value;
+                var flag3 = string.IsNullOrWhiteSpace(player);
+                if (!flag3)
+                {
+                    var alpha = Mathf.Clamp01(timeLeft / 2f);
+                    style.normal.textColor = new Color(1f, 0.8f, 0f, alpha);
+                    var text = player + " is speaking...";
+                    var textSize = style.CalcSize(new GUIContent(text));
+                    var x = Screen.width - 20 - textSize.x;
+                    var y = yStart - index * (height + padding);
+                    GUI.Label(new Rect(x, y, textSize.x, height), text, style);
+                    index++;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            See_who_is_talking.Logger.LogError(string.Format("[VoiceDisplayManager.OnGUI] Exception: {0}", ex));
+        }
+    }
+
+    // Token: 0x06000015 RID: 21 RVA: 0x0000230C File Offset: 0x0000050C
+    public static void SetSpeakingPlayer(string name)
+    {
+        if (instance == null || string.IsNullOrWhiteSpace(name)) return;
+        name = name.Trim();
+        var obj = instance.dictLock;
+        lock (obj)
+        {
+            var flag3 = string.IsNullOrWhiteSpace(name);
+            if (flag3)
+            {
+                See_who_is_talking.Logger.LogWarning("SetSpeakingPlayer called with null or empty name.");
+            }
+            else
+            {
+                var flag4 = instance.speakingPlayers.ContainsKey(name);
+                if (flag4)
+                    instance.speakingPlayers[name] = 2f;
+                else
+                    instance.speakingPlayers.Add(name, 2f);
+            }
+        }
+    }
 }
