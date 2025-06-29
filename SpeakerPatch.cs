f1e7e0c339880e21345dfeb36604424c56497a00
Rob Nelson <nexisentertainment@gmail.com>
2025-06-29 01:02:29 -0700

    Decompiled code, removed compiler structures

---
+++ SpeakerPatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
+using Photon.Pun;
+using Photon.Realtime;
+using Photon.Voice.Unity;
+
+namespace See_who_is_talking
+{
+	// Token: 0x02000008 RID: 8
+	[HarmonyPatch(typeof(Speaker), "OnAudioFrame")]
+	internal class SpeakerPatch
+	{
+		// Token: 0x06000012 RID: 18 RVA: 0x0000220C File Offset: 0x0000040C
+		// ReSharper disable once InconsistentNaming
+		private static void Prefix(Speaker __instance)
+		{
+			if (__instance == null || __instance.RemoteVoice == null) return;
+			int playerId = __instance.RemoteVoice.PlayerId;
+			string? name;
+			try
+			{
+				Room currentRoom = PhotonNetwork.CurrentRoom;
+				Player? player = currentRoom?.GetPlayer(playerId, false);
+				name = player?.NickName;
+				if (string.IsNullOrWhiteSpace(name))
+				{
+					name = string.Format("Player {0}", playerId);
+				}
+			}
+			catch
+			{
+				name = string.Format("Player {0}", playerId);
+			}
+			VoiceDisplayManager.SetSpeakingPlayer(name);
+		}
+	}
+}
